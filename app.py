33ee1dc40ecc45af7ca0e7f272eece4591f0d9b1
Ojas Desaai <emailojasdesai@gmail.com>
2026-09-08 22:27:47 -0700

    Final update to app.py before initial deployment

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import base64
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(page_title="Digital Jaltarang", layout="wide")
+st.set_page_config(page_title="Digital Jaltarang Bowls", layout="wide")
 
 st.markdown("""
 <style>
